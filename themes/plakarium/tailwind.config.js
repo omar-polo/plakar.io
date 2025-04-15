61abe0581d036a39a68f05eeb28caecc7c7a8c3d
misterflop <julien@plakar.io>
2025-04-15 13:59:43 +0200

    First article hugo integration

--- themes/plakarium/tailwind.config.js
+++ themes/plakarium/tailwind.config.js
@@ -1,6 +1,7 @@
 console.log("Tailwind config bien chargée ✅");
 
 import fluid, { extract, screens, fontSize } from 'fluid-tailwind'
+import typography from '@tailwindcss/typography'
 
 module.exports = {
     content: {
@@ -48,9 +49,126 @@ module.exports = {
                     900: '#1C1C1E', // fond section "features"
                 },
             },
+            typography: ({ theme }) => ({
+                DEFAULT: {
+                    css: {
+                        "--tw-prose-body": theme("colors.neutral.700 / 1"),
+                        "--tw-prose-headings": theme("colors.neutral.800 / 1"),
+                        "--tw-prose-lead": theme("colors.neutral.500 / 1"),
+                        "--tw-prose-links": theme("colors.primary.600 / 1"),
+                        "--tw-prose-bold": theme("colors.neutral.900 / 1"),
+                        "--tw-prose-counters": theme("colors.neutral.800 / 1"),
+                        "--tw-prose-bullets": theme("colors.neutral.500 / 1"),
+                        "--tw-prose-hr": theme("colors.neutral.200 / 1"),
+                        "--tw-prose-quotes": theme("colors.neutral.700 / 1"),
+                        "--tw-prose-quote-borders": theme("colors.primary.200 / 1"),
+                        "--tw-prose-captions": theme("colors.neutral.500 / 1"),
+                        "--tw-prose-code": theme("colors.secondary.700 / 1"),
+                        "--tw-prose-pre-code": theme("colors.neutral.700 / 1"),
+                        "--tw-prose-pre-bg": theme("colors.neutral.50 / 1"),
+                        "--tw-prose-th-borders": theme("colors.neutral.500 / 1"),
+                        "--tw-prose-td-borders": theme("colors.neutral.300 / 1"),
+                        "--tw-prose-invert-body": theme("colors.neutral.300 / 1"),
+                        "--tw-prose-invert-headings": theme("colors.neutral.50 / 1"),
+                        "--tw-prose-invert-lead": theme("colors.neutral.500 / 1"),
+                        "--tw-prose-invert-links": theme("colors.primary.400 / 1"),
+                        "--tw-prose-invert-bold": theme("colors.neutral.DEFAULT / 1"),
+                        "--tw-prose-invert-counters": theme("colors.neutral.400 / 1"),
+                        "--tw-prose-invert-bullets": theme("colors.neutral.600 / 1"),
+                        "--tw-prose-invert-hr": theme("colors.neutral.500 / 1"),
+                        "--tw-prose-invert-quotes": theme("colors.neutral.200 / 1"),
+                        "--tw-prose-invert-quote-borders": theme("colors.primary.900 / 1"),
+                        "--tw-prose-invert-captions": theme("colors.neutral.400 / 1"),
+                        "--tw-prose-invert-code": theme("colors.secondary.400 / 1"),
+                        "--tw-prose-invert-pre-code": theme("colors.neutral.200 / 1"),
+                        "--tw-prose-invert-pre-bg": theme("colors.neutral.700 / 1"),
+                        "--tw-prose-invert-th-borders": theme("colors.neutral.500 / 1"),
+                        "--tw-prose-invert-td-borders": theme("colors.neutral.700 / 1"),
+                        a: {
+                            textDecoration: "none",
+                            textDecorationColor: theme("colors.primary.300 / 1"),
+                            fontWeight: "500",
+                            "&:hover": {
+                                color: theme("colors.primary.600 / 1"),
+                                textDecoration: "none",
+                                borderRadius: "0.09rem",
+                            },
+                        },
+                        "a code": {
+                            color: "var(--tw-prose-code)",
+                        },
+                        kbd: {
+                            backgroundColor: theme("colors.neutral.200 / 1"),
+                            padding: "0.1rem 0.4rem",
+                            borderRadius: "0.25rem",
+                            fontSize: "0.9rem",
+                            fontWeight: "600",
+                        },
+                        mark: {
+                            color: theme("colors.neutral.800 / 1"),
+                            backgroundColor: theme("colors.primary.600 / 1"),
+                            padding: "0.1rem 0.2rem",
+                            borderRadius: "0.25rem",
+                        },
+                        code:{
+                            backgroundColor: theme("colors.neutral.50 / 1"),
+                            paddingTop: "3px",
+                            paddingBottom: "3px",
+                            paddingLeft: "5px",
+                            paddingRight: "5px",
+                            borderRadius: "0.25rem",
+                        },
+                        'code::before': {
+                            display: 'none'
+                        },
+                        'code::after': {
+                            display: 'none'
+                        },
+                        'p::before': {
+                            display: 'none'
+                        },
+                        'p::after': {
+                            display: 'none'
+                        },
+                        'a.active': {
+                            "text-decoration-color": theme("colors.primary.600 / 1"),
+                        },
+                        'p.active': {
+                            "text-decoration-color": theme("colors.primary.600 / 1"),
+                        }
+                    },
+                },
+                invert: {
+                    css: {
+                        a: {
+                            textDecorationColor: theme("colors.neutral.600 / 1"),
+                            "&:hover": {
+                                color: theme("colors.primary.400 / 1"),
+                            },
+                        },
+                        kbd: {
+                            color: theme("colors.neutral.200 / 1"),
+                            backgroundColor: theme("colors.neutral.700 / 1"),
+                        },
+                        mark: {
+                            backgroundColor: theme("colors.primary.400 / 1"),
+                        },
+                        code:{
+                            backgroundColor: theme("colors.neutral.700 / 1"),
+                        },
+                        'a.active': {
+                            "text-decoration-color": theme("colors.primary.400 / 1")
+                        },
+                        'p.active': {
+                            "text-decoration-color": theme("colors.primary.400 / 1")
+                        }
+                    },
+                },
+            }),
         },
     },
     plugins: [
+        typography,
         fluid
     ],
 }
